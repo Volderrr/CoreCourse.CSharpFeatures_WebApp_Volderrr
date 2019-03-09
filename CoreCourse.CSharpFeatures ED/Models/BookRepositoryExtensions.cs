389a6d7bfdd7305539e6503fc1b4e45b231aaa67
Volderrr <eline_devolder@hotmail.com>
2019-03-09 18:20:12 +0100

    Add a new class BookRepositoryExtensions

---
+++ CoreCourse.CSharpFeatures ED/Models/BookRepositoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCourse.CSharpFeatures_ED.Models
+{
+    public static class BookRepositoryExtensions
+    {
+        public static int TotalPages(this BookRepository bookRepository)
+        {
+            int totalPages = 0;
+            foreach (Book book in bookRepository.Books)
+                totalPages += book?.Pages ?? 0;
+
+            return totalPages;
+        }
+    }
+}
