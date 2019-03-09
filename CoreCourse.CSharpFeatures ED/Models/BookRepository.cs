cdbfb85a541e807716f8b3fce1f23eaa8f1896ee
Volderrr <eline_devolder@hotmail.com>
2019-03-09 19:34:11 +0100

    Implementeer IEnumerable

--- CoreCourse.CSharpFeatures ED/Models/BookRepository.cs
+++ CoreCourse.CSharpFeatures ED/Models/BookRepository.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CoreCourse.CSharpFeatures_ED.Models
 {
-    public class BookRepository
+    public class BookRepository : IEnumerable<Book>
     {
         public IEnumerable<Book> Books { get; set; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return this.Books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
     }
 }
